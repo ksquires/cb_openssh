d3035318d9e7ae778400c7d5669c0f4c6c4d549b
Kent Squires <ksquires@gmail.com>
2018-02-28 19:52:59 -0600

    testing

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,28 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe file('/etc/ssh/sshd_config') do
+  it { should exist }
+  its('content') { should match /^AddressFamily inet/ }
+  its('content') { should match /^AllowTcpForwarding yes/ }
+  its('content') { should match /^AuthorizedKeysFile %h\/.ssh\/authorized_keys/ }
+  its('content') { should match /^ChallengeResponseAuthentication no/ }
+  its('content') { should match /^GssapiAuthentication no/ }
+  its('content') { should match /^GssapiCleanUpCredentials yes/ }
+  its('content') { should match /^HostKey \/etc\/ssh\/ssh_host_rsa_key/ }
+  its('content') { should match /^HostKey \/etc\/ssh\/ssh_host_ecdsa_key/ }
+  its('content') { should match /^HostKey \/etc\/ssh\/ssh_host_ed25519_key/ }
+  its('content') { should match /^LogLevel VERBOSE/ }
+  its('content') { should match /^PasswordAuthentication yes/ }
+  its('content') { should match /^Protocol 2/ }
+  its('content') { should match /^RevokedKeys \/etc\/ssh\/revoked_keys/ }
+  its('content') { should match /^Subsystem sftp \/usr\/libexec\/openssh\/sftp-server/ }
+  its('content') { should match /^SyslogFacility AUTHPRIV/ }
+  its('content') { should match /^TrustedUserCAKeys \/etc\/ssh\/ca_keys/ }
+  its('content') { should match /^UsePAM yes/ }
+  its('content') { should match /^UsePrivilegeSeparation sandbox/ }
+  its('content') { should match /^X11DisplayOffset 10/ }
+  its('content') { should match /^X11Forwarding yes/ }
+  its('content') { should match /^X11UseLocalhost yes/ }
 end
